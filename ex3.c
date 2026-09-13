b0843661d7db27a5651f3b85847e273796186f2e
Taras Bialyk <70756830+Taras265@users.noreply.github.com>
2026-09-13 12:46:34 +0300

    Update ex3.c

--- ex3.c
+++ ex3.c
@@ -1,68 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <signal.h>
 
-void printFileContent(const char *filename, int step) {
-    FILE *fp = fopen(filename, "r");
+int main(void){
+    (void)signal(SIGINT, SIG_IGN);
 
-    if (fp == NULL) {
-        printf("Error: couldnt open file \"%s\"\n\n", filename);
-        return;
-    }
+    (void)signal(SIGQUIT, SIG_IGN);
 
-    printf("\nFILE: %s\n", filename);
-
-    int c;
-    long count = 0;
-
-    while ((c = fgetc(fp)) != EOF) {
-        count++;
-        if (step <= 1) {
-            putchar(c);
-        } else if (count % step == 0) {
-            putchar(c);
-        }
-    }
-
-    putchar('\n');
-    printf("\n");
-
-    fclose(fp);
-}
-
-int main(int argc, char *argv[]) {
-
-    if (argc < 2) {
-        fprintf(stderr, "Error: no one file was entered\n");
-        exit(EXIT_FAILURE);
-    }
-
-    int step = 1;
-    int start = 1;
-
-    if (strcmp(argv[1], "-Z") == 0) {
-        if (argc < 4) {
-            fprintf(stderr, "Error: after flag -Z the positive integer number needed\n");
-            exit(EXIT_FAILURE);
-        }
-
-        step = atoi(argv[2]);
-        if (step <= 0) {
-            fprintf(stderr, "Error: after flag -Z the positive integer number needed\n");
-            exit(EXIT_FAILURE);
-        }
-
-        start = 3;
-    }
-
-    if (start >= argc) {
-        fprintf(stderr, "Error: no one file to processing entered\n");
-        exit(EXIT_FAILURE);
-    }
-
-    for (int i = start; i < argc; i++) {
-        printFileContent(argv[i], step);
-    }
-
+    while(1);
     return 0;
 }
